ce9920e287ddd314e5ddc63510a0a81a2671905f
Ironboy <ironboy@users.noreply.github.com>
2024-03-31 10:36:36 +0200

    minor change

--- Backend/FileServer.cs
+++ Backend/FileServer.cs
@@ -3,12 +3,12 @@ using Microsoft.Extensions.FileProviders;
 public class FileServer
 {
 
-  public FileServer(WebApplication app, string[] path)
+  public FileServer(WebApplication app, string path)
   {
     app.UseFileServer(new FileServerOptions
     {
       FileProvider = new PhysicalFileProvider(
-        Path.Combine(Directory.GetCurrentDirectory())
+        Path.Combine(Directory.GetCurrentDirectory(), "..", path)
       )
     });
   }
