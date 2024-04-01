29fa8f263f11fdeecca58a155111a63eda2e34c0
Ironboy <ironboy@users.noreply.github.com>
2024-04-01 13:39:10 +0200

    better comment

--- Backend/src/SessionHandle.cs
+++ Backend/src/SessionHandle.cs
@@ -1,48 +1,46 @@
 public static class SessionHandle
 {
-
-    private static string GetCookieValue(HttpContext context)
+  private static string GetCookieValue(HttpContext context)
+  {
+    // Remember: context.Items is request specific!
+    var value = context.Items["cookieValue"] as string;
+    if (value == null)
     {
-        // Remember: context.Items is request specific!
-        var value = context.Items["cookieValue"] as string;
-        if (value == null)
-        {
-            // This is the first call during this request to
-            // GetCookieValue, so get the cookieValue from the request!
-            context.Request.Cookies.TryGetValue("session", out value);
-        }
-        if (value == null)
-        {
-            // There is no cookie set yet for this client. Set one!
-            value = Guid.NewGuid().ToString();
-            context.Response.Cookies.Append("session", value);
-        }
-        // Check if a session with this cookie as id is stored in the DB
-        var exists = SQLQuery.RunOne(
-            "SELECT * FROM sessions WHERE id = $id",
-            "id", value
-        );
-        if (exists == null)
-        {
-            // Otherwise store a new session in the DB
-            SQLQuery.RunOne(
-                 "INSERT INTO sessions(id) VALUES($id)",
-                 "id", value
-             );
-        }
-        context.Items["cookieValue"] = value;
-        return value;
+      // This is the first call during this request to
+      // GetCookieValue, so get the cookieValue from the request!
+      context.Request.Cookies.TryGetValue("session", out value);
     }
-
-    public static void Touch(HttpContext context)
+    if (value == null)
     {
-        var cookieValue = GetCookieValue(context);
-        // Touch the session with a new modified time
-        SQLQuery.RunOne(
-            @"UPDATE sessions SET modified = DATETIME('now')
+      // There is no cookie set yet for this client. Set one!
+      value = Guid.NewGuid().ToString();
+      context.Response.Cookies.Append("session", value);
+    }
+    // Check if the DB has a session with this cookie as id
+    var exists = SQLQuery.RunOne(
+        "SELECT * FROM sessions WHERE id = $id",
+        "id", value
+    );
+    if (exists == null)
+    {
+      // Otherwise store a new session in the DB
+      SQLQuery.RunOne(
+           "INSERT INTO sessions(id) VALUES($id)",
+           "id", value
+       );
+    }
+    context.Items["cookieValue"] = value;
+    return value;
+  }
+
+  public static void Touch(HttpContext context)
+  {
+    var cookieValue = GetCookieValue(context);
+    // Touch the session - set modified to now!
+    SQLQuery.RunOne(
+        @"UPDATE sessions SET modified = DATETIME('now')
               WHERE id = $id",
-            "id", cookieValue
-        );
-    }
-
+        "id", cookieValue
+    );
+  }
 }
