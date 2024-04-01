bb99a7ddd7db5ee43a4ff666850bfcca8a663a24
Ironboy <ironboy@users.noreply.github.com>
2024-04-01 03:41:06 +0200

    moving src code to src folder

---
+++ Backend/src/App.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Always throw custom errors (in dev and production)
+builder.Services.Configure<RouteHandlerOptions>
+ (o => o.ThrowOnBadRequest = true);
+
+var app = builder.Build();
+
+new Middleware(app, "ironboy's minimal API server");
+new ErrorHandler(app);
+new FileServer(app, "Frontend");
+new LoginHandler(app);
+new REST(app);
+SessionHandler.DeleteOldSessions(2);
+
+app.Run("http://localhost:3001");
