080161ec79112f681e801dcedfb6f50cbe38df2b
Ironboy <ironboy@users.noreply.github.com>
2024-04-02 03:56:41 +0200

    moving small middleware into app

--- Backend/src/App.cs
+++ Backend/src/App.cs
@@ -1,16 +1,27 @@
+var serverName = "ironboy's minimal API server";
 var builder = WebApplication.CreateBuilder(args);
 
-// Always throw custom errors (in dev and production)
+// Always throw custom errors, in dev and production
 builder.Services.Configure<RouteHandlerOptions>
     (o => o.ThrowOnBadRequest = true);
 
 var app = builder.Build();
 
-DoOnEveryRequest.Start(app, "ironboy's minimal API server");
+// Middleware to set server name response header 
+// and touch the user session with new timestamp
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Append("Server", serverName);
+    Session.Touch(context);
+    await next(context);
+});
+
+// Set up routes, error handling and session purging
 ErrorHandler.Start(app);
 FileServer.Start(app, "..", "Frontend");
 LoginRoutes.Start(app);
 REST.Start(app);
 Session.DeleteOldSessions(2);
 
+// Start the server on port 3001
 app.Run("http://localhost:3001");
