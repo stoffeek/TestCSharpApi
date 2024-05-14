5850e9e93fac286ce25f166524119ffb3d8146ac
stoffeek <stoffeek@gmail.com>
2024-05-14 10:06:18 +0200

    endpoint testing

--- Backend/src/App.cs
+++ Backend/src/App.cs
@@ -6,7 +6,7 @@ Globals = Obj(new
     aclOn = true,
     isSpa = true,
     port = 3001,
-    serverName = "Ironboy's Minimal API Server",
+    serverName = "Stoffes Stuuffz   ",
     frontendPath = FilePath("..", "Frontend"),
     sessionLifeTimeHours = 2
 });
